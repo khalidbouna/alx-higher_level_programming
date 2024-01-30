b16ea9a049c719d6d5bef9618eb0123ebfc22260
khalidbouna <bouna.ck@gmail.com>
2024-01-30 15:36:49 -0800

    m

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def magic_string(H=None):
     if H is None:
-        H = ["BestSchool"]
-    else:
-        H.append("BestSchool")
+        H = []
+    H.append("BestSchool")
     return ", ".join(H)
