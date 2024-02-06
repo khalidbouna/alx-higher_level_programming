b261f4ab33f09475427f4fed589fd5621271f9d7
khalidbouna <bouna.ck@gmail.com>
2024-02-06 11:40:05 -0800

    m

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+def read_file(filename=""):
+    with open(filename, encoding='utf-8') as file:
+        print(file.read(), end='')
+
+if __name__ == "__main__":
+    read_file("my_file_0.txt")
