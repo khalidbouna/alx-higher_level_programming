9cb7314a58f256ab4b5c07e32a92a13f23451c89
khalidbouna <bouna.ck@gmail.com>
2024-02-06 13:32:37 -0800

    ma

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" Student module
+"""
+
+class Student:
+    """ Defines a student """
+
+    def __init__(self, first_name, last_name, age):
+        """ Initializes a Student instance """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """ Retrieves a dictionary representation of a Student instance """
+        if attrs is None:
+            return self.__dict__.copy()
+        else:
+            return {attr: getattr(self, attr) for attr in attrs if hasattr(self, attr)}
