4c1b1e8893d842601686c5889123579d27848140
khalidbouna <bouna.ck@gmail.com>
2024-02-06 13:11:07 -0800

    ma

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -1,20 +1,38 @@
 #!/usr/bin/python3
 """
-Defines a Student class
+Contains a class Student
 """
 
+
 class Student:
-    """Represents a student"""
+    """
+    Defines a student
+    """
 
     def __init__(self, first_name, last_name, age):
-        """Initializes a Student instance with first name, last name, and age"""
+        """
+        Initializes a Student instance
+        """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-        """Returns the dictionary representation of a Student instance"""
+        """
+        Retrieves a dictionary representation of a Student instance
+        """
         if attrs is None:
             return self.__dict__
-        else:
-            return {attr: getattr(self, attr) for attr in attrs if hasattr(self, attr)}
+        return {attr: getattr(self, attr) for attr in attrs if hasattr(self, attr)}
+
+if __name__ == "__main__":
+    student_1 = Student("John", "Doe", 23)
+    student_2 = Student("Bob", "Dylan", 27)
+
+    j_student_1 = student_1.to_json()
+    j_student_2 = student_2.to_json(['first_name', 'age'])
+    j_student_3 = student_2.to_json(['middle_name', 'age'])
+
+    print(j_student_1)
+    print(j_student_2)
+    print(j_student_3)
