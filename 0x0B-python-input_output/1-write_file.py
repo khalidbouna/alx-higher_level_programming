cc06fba705a9c5628a6ebccc8e73b58769219060
khalidbouna <bouna.ck@gmail.com>
2024-02-06 11:46:03 -0800

    m

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,13 +1,10 @@
 #!/usr/bin/python3
+"""
+Writes a string to a text file and returns the number of characters written.
+"""
+
 
 def write_file(filename="", text=""):
-    """
-    Writes a string to a text file (UTF8) and returns the number of characters written.
-    """
-    with open(filename, 'w', encoding='utf-8') as f:
-        nb_characters = f.write(text)
-    return nb_characters
-
-if __name__ == "__main__":
-    nb_characters = write_file("my_first_file.txt", "This School is so cool!\n")
-    print(nb_characters)
+    """Writes text to filename and returns the number of characters written."""
+    with open(filename, 'w', encoding='utf-8') as file:
+        return file.write(text)
