de1bfe3ff85a4e6988b10861d570bdf24cf9faf8
khalidbouna <bouna.ck@gmail.com>
2024-02-06 11:47:20 -0800

    m

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
-def read_file(filename=""):
-    with open(filename, encoding='utf-8') as file:
-        print(file.read(), end='')
+"""
+Reads a text file (UTF8) and prints it to stdout.
+"""
 
-if __name__ == "__main__":
-    read_file("my_file_0.txt")
+
+def read_file(filename=""):
+    """Reads filename and prints its content to stdout."""
+    with open(filename, 'r', encoding='utf-8') as file:
+        print(file.read(), end="")
